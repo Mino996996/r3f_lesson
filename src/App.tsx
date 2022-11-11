b3fe8ae5ea010f02e278856679455e16716af144
minoru akahori <minoruaka@gmail.com>
2022-11-11 14:15:18 +0900

    wip: practice to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './App.css';
+// import { useLoader } from '@react-three/fiber';
+// import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader.js';
+import { Canvas } from '@react-three/fiber';
+// import Box from './BoxSample';
 
 const App: React.FC = () => {
-  return <div className="App">test</div>;
+  return (
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/BoxSample.tsx
@@ -0,0 +1,25 @@
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber/';
+
+// const Box: React.FC = (props) => {
+//   const mesh = useRef(null);
+//   const [hovered, setHover] = useState(false);
+//   const [active, setActive] = useState(false);
+//   useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+//   return (
+//     <mesh
+//       {...props}
+//       ref={mesh}
+//       scale={active ? 1.5 : 1}
+//       onClick={(event) => setActive(!active)}
+//       onPointerOver={(event) => setHover(true)}
+//       onPointerOut={(event) => setHover(false)}
+//     >
+//       {/* eslint-disable-next-line react/no-unknown-property */}
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+//     </mesh>
+//   );
+// };
+//
+// export default Box;
